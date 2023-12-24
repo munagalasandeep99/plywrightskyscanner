e33baa4355959675e92d4d85140c65e663a8b9a7
Sandeep Reddy Munagala <129391713+munagalasandeep99@users.noreply.github.com>
2023-12-24 18:10:39 +0100

    Update skyscanner.spec.js

--- tests/skyscanner.spec.js
+++ tests/skyscanner.spec.js
@@ -1,49 +1,48 @@
 const { test, expect } = require('@playwright/test');
 
-test('basic test', async ({ page }) => {
+test('My first test', async ({ page }) => {
   await page.goto('https://www.skyscanner.se/');
   await page.getByText('Ok', { exact: true }).click();
-
-  // Setting origin
-  await page.click('id=originInput-input');
-  await page.fill('id=originInput-input', 'Stockholm');
+  await page
+    .getByRole('combobox', { name: 'Ange den stad du flyger ifrån' })
+    .click();
+  await page
+    .getByRole('combobox', { name: 'Ange den stad du flyger ifrån' })
+    .fill('stoc');
   await page.getByText('Stockholm Arlanda (ARN)').click();
-
-  // Setting destination
-  await page.click('id=destinationInput-input');
-  await page.getByRole('combobox', { name: '. Ange din destination eller' }).click();
+  await page
+    .getByRole('combobox', { name: '. Ange din destination eller' })
+    .click();
   await page.getByRole('button', { name: 'Utforska överallt' }).click();
-
-  // Setting dates and adults
-  await page.getByLabel('fredag 22 december 2023. Välj').click();
+  await page.getByLabel('söndag 31 december 2023. Välj').click();
   await page.getByLabel('lördag 6 januari 2024. Välj').click();
+  await page.getByTestId('traveller-button').click();
   await page.getByLabel('VuxnaÖver 16 år').click();
   await page.getByTestId('desktop-travellerselector').getByRole('button', { name: 'Sök' }).click();
-
-  // Selecting flight options
+  const text = await page.getByRole('link', { name: 'Direktflyg till Sverige från' }).textContent();
+  const price1 = text.match(/\d+/g);
+    console.log('Price of Flight in first link');
+    console.log(price1[0]);
+  
   await page.getByRole('link', { name: 'Direktflyg till Sverige från' }).click();
-  const firstFlightOption = page.locator('.BpkBackgroundImage_bpk-background-image__img__NDhjM').first();
-  await firstFlightOption.click();
+  await page.waitForSelector('a[data-testid="flights-link"]');
+  
+  // Extract the aria-label content
+  const ariaLabel = await page.$eval('a[data-testid="flights-link"]', (element) => {
+    return element.getAttribute('aria-label');
+  });
 
-  // Working with the new popup page
-  const popupPage = await page.waitForEvent('popup');
-  const priceButton = popupPage.getByRole('button', { name: 'Flygalternativ 2: Total' });
-  const priceText = await priceButton.textContent();
-  const priceParts = priceText.split(' ');
-  const initialPrice = priceParts[4] + ' ' + priceParts[5].substring(0, 3);
-  console.log(`cost of ticket in initial stage: ${initialPrice}`);
+  const price2 = ariaLabel.match(/\d+/g);
+  console.log('Price of Flight after clicking first link');
+    console.log(price2[0]);
+    if (price2[0] === price1[0]) {
+        console.log('Testcase Passed: Prices are same');
+      } else {
+        console.log('Testcase Failed: Prices are different, berfore and after');
+      }
 
-  // Checking the price after clicking
-  await priceButton.getByRole('button').click();
-  const pricingContainer = popupPage.getByTestId('pricing-item-container');
-  const finalPriceText = await pricingContainer.getByText('SEK').textContent();
-  console.log(`cost  after clicking the first link: ${finalPriceText}`);
+  await page.locator('.BpkBackgroundImage_bpk-background-image__img__NDhjM').first().click();
 
-  // Asserting the price comparison
-  if (initialPrice === finalPriceText) {
-    console.log('test passed');
-  }
-  else {
-    console.log('test failed');
-  }
+
+
 });
